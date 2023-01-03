fae38f61aac9ffa4df323642dadb9c04c4121a96
kylaj <20098155@mail.wit.ie>
2023-01-03 16:08:55 +0000

    hi

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.bakeryapplication {
     requires javafx.controls;
     requires javafx.fxml;
+    requires xstream;
 
     opens com.example.bakeryapplication to javafx.fxml;
     exports com.example.bakeryapplication;
